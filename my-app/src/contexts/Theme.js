50ce100f2eeddd1975712163d311a0de591935d8
Ycls <your-email@example.com>
2024-07-06 15:49:25 +0530

    context-Api

---
+++ my-app/src/contexts/Theme.js
@@ -0,0 +1,15 @@
+import {createContext, useContext } from "react"
+
+ export const ThemeContext = createContext({
+    themeMode: "light",
+    lightTheme:() => {},
+     darkTheme: () => {},
+
+  
+ })
+
+ export const ThemeProider = ThemeContext.Provider
+
+ export default function useTheme() {
+    return useContext(ThemeContext)
+}
